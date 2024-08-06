be2d2f1a3f2627696b8873935940bf4bd72dd7d8
Mynamisboseong <wow456869@gmail.com>
2024-08-06 17:08:44 +0900

    bind 사용

--- Queez_commentary/number4.js
+++ Queez_commentary/number4.js
@@ -19,3 +19,56 @@ var theOralCigrettes = new RockBand([
 ]);
 
 theOralCigarettes.perform();
+
+// this와 scope에 대한 문제 중 하나이다. 기본적으로 JS는 함수스코프에 유의를 해야 한다. 4번째 줄의 this는 무엇일까?
+// 2, 3번쨰 줄의 this는 5번째 줄의 this를 가르키지 않는다. 2,3번째 this는 RockBand fuction에 있는 this고, 5번째의 this는 setTimeout function에 있는 this다. 즉 this가 RockBand의 this를 가르키지 않는다.
+
+
+//해결법 1. Arrow function
+//arrow function은 자체로 function 스코프를 만들지 않고 해당 arrowfuction 상위에 있는 스코프를 찾아간다. 자연스럽게 this가 RockBand의 this를 가르키게 된다.
+function RockBand(members) {
+    this.members = members;
+    this.perform = fuction() {
+        setTimeout(() => {
+            this.members.forEach(function(member){
+                member.perform();
+            })
+        },1000)
+    }
+}
+
+var theOralCigrettes = new RockBand([
+    {
+        name: 'takuya',
+        perform: function() {
+            console.log('sing: a e u i a e u i')
+        }
+    }
+]);
+
+theOralCigarettes.perform();
+
+//해결법 2. bind 사용
+//bind(this)의 this는 RockBand내부에 있는 this를 가르킨다. 왜냐하면, RockBand function 스코프 안에 있으니까! 그리고 bind는 bind를 통해 생성된 새로운 함수를, RockBand function의 this와 연결시켜준다.
+//this가 RockBand의 this를 가르키게 된다.
+function RockBand(members) {
+    this.members = members;
+    this.perform = fuction() {
+        setTimeout(function() {
+            this.members.forEach(function(member){
+                member.perform();
+            })
+        }.bind(this),1000)
+    }
+}
+
+var theOralCigrettes = new RockBand([
+    {
+        name: 'takuya',
+        perform: function() {
+            console.log('sing: a e u i a e u i')
+        }
+    }
+]);
+
+theOralCigarettes.perform();
